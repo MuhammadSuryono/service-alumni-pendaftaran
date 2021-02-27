50cf4d4c9cb474805d218dac73b0e0aa2201e300
Muhammad Suryono <yono@dmk.co.id>
2021-02-28 00:01:13 +0700

    Add response custome json

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,13 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public function CommonResponse(bool $status, string $message, $data, int $statusCode)
+    {
+        $response = [
+            "status" => $status,
+            "message" => $message,
+            "data" => $data,
+        ];
+        return response()->json($response, 200);
+    }
 }
